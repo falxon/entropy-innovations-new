9b75ced36eed6da9bd0ffd390657d5badb467620
Ember <falconsflight@riseup.net>
2018-04-02 16:32:04 +0100

    added popovers to form and made projects field required when visible

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,16 +1,36 @@
 $(document).ready(function(){
    if(window.location.pathname != "/current"){
      $("#project").hide();
+     $("#project2").removeAttr("required");
+
    }else{
      $("#project").show();
+     $("#project2").attr("required", "true");
    }
 
    $("#topic").change(function(){
      if($("#topic").val() != "current"){
        $("#project").hide();
+       $("#project2").removeAttr("required");
      }
      else {
        $("#project").show();
+       $("#project2").attr("required", "true");
      }
    });
 });
+
+$(function () {
+  $('.example-popover').popover({
+    container: 'body'
+  })
+})
+
+$('.popover-dismiss').popover({
+  trigger: 'focus'
+})
+
+$('#name').popover("click");
+$('#email').popover("click");
+$('#subject').popover("click");
+$('#project2').popover("click");
