8a8c292bd477ec5d8066263e38205c5872d002ad
Ember <falconsflight@riseup.net>
2018-04-18 10:33:08 +0100

    added https section to site

--- php-include/home.inc.php
+++ php-include/home.inc.php
@@ -23,19 +23,29 @@ $home["main"][0]["section"][1]["subtitle"][0]["class"] = "mt-3";
 $home["main"][0]["section"][1]["paragraph"][0]["text"] = "We can offer you the full package of web development. We'll take care of buying your domain name for you, setting up an email address, building your website, and setting up hosting. Then for a modest yearly or monthtly fee, we can be on hand to do any maintenance that your site needs, including domain renewals. Or, if you'd prefer, we can transfer the responsibility over to you. Throughout this process, you'll be taken care of by one of our highly-trained and experienced web developers, who can talk to you in depth about what you want your website to do for you.";
 $home["main"][0]["section"][1]["paragraph"][1]["text"] = "Prices can be negotiated based on your needs and estimated hours of work involved.";
 
-$home["main"][0]["section"][2]["subtitle"][0]["subtext"] = "What about mobile sites?";
+$home["main"][0]["section"][2]["subtitle"][0]["subtext"] = "What about HTTPS?";
 $home["main"][0]["section"][2]["subtitle"][0]["num"] = "5";
-$home["main"][0]["section"][2]["paragraph"][0]["text"] = "We employ cutting-edge web technologies to build websites with responsiveness in mind. This means that your site will dynamically adapt to whatever device it is viewed on, regardless of size.";
+$home["main"][0]["section"][2]["paragraph"][0]["text"] = "You may have heard that Google Chrome is going to start marking sites that don't use HTTPS as 'Not Secure' in the URL bar - <a href='https://www.theregister.co.uk/2018/02/08/google_chrome_http_shame/'>starting this July</a>. Our web hosting comes with an HTTPS certificate <i>by default</i> with no setup fee.";
+$home["main"][0]["section"][2]["paragraph"][0]["class"] = "links-white";
+$home["main"][0]["section"][2]["paragraph"][1]["text"] = "Are you currently tied in with a 'Build Your Own Website' service that is threatening to make you pay money for HTTPS? Talk to us and we can painlessly migrate your website to our hosting platform, where we won't try to make extra money off you for bare minimum security.";
 
+
+$home["main"][0]["section"][3]["subtitle"][0]["subtext"] = "What about mobile sites?";
 $home["main"][0]["section"][3]["subtitle"][0]["num"] = "5";
-$home["main"][0]["section"][3]["subtitle"][0]["subtext"] = "What do you need to do?";
-$home["main"][0]["section"][3]["paragraph"][0]["text"] = "You need to choose a domain name for your website. With the help of your web developer, decide on a structure, and write the content. You'll be given regular progress updates as the site takes form, and given an opportunity to make modifications at the end.";
+$home["main"][0]["section"][3]["paragraph"][0]["text"] = "We employ cutting-edge web technologies to build websites with responsiveness in mind. This means that your site will dynamically adapt to whatever device it is viewed on, regardless of size.";
+
+
 
 $home["main"][0]["section"][4]["subtitle"][0]["num"] = "5";
-$home["main"][0]["section"][4]["subtitle"][0]["subtext"] = "What Next?";
-$home["main"][0]["section"][4]["paragraph"][0]["text"] = "If you're interested in finding out more, why not take a look at our <a href='/portfolio' class='links-normal'>portfolio</a>, where you can find some examples of the work we've done. Otherwise, send us a message via the <a href='/contact'>contact form</a> so you can get started.";
+$home["main"][0]["section"][4]["subtitle"][0]["subtext"] = "What do you need to do?";
+$home["main"][0]["section"][4]["paragraph"][0]["text"] = "You need to choose a domain name for your website. With the help of your web developer, decide on a structure, and write the content. You'll be given regular progress updates as the site takes form, and given an opportunity to make modifications at the end.";
 
-$home["main"][0]["section"][4]["buttons"][0]["button"][0]["url"] = "portfolio";
-$home["main"][0]["section"][4]["buttons"][0]["button"][0]["text"] = "Portfolio";
-$home["main"][0]["section"][4]["buttons"][0]["button"][1]["url"] = "contact";
-$home["main"][0]["section"][4]["buttons"][0]["button"][1]["text"] = "Contact";
+$home["main"][0]["section"][5]["subtitle"][0]["num"] = "5";
+$home["main"][0]["section"][5]["subtitle"][0]["subtext"] = "What Next?";
+$home["main"][0]["section"][5]["paragraph"][0]["text"] = "If you're interested in finding out more, why not take a look at our <a href='/portfolio' class='links-normal'>portfolio</a>, where you can find some examples of the work we've done. Otherwise, send us a message via the <a href='/contact'>contact form</a> so you can get started.";
+$home["main"][0]["section"][5]["paragraph"][0]["class"] = "links-normal";
+
+$home["main"][0]["section"][6]["buttons"][0]["button"][0]["url"] = "portfolio";
+$home["main"][0]["section"][6]["buttons"][0]["button"][0]["text"] = "Portfolio";
+$home["main"][0]["section"][6]["buttons"][0]["button"][1]["url"] = "contact";
+$home["main"][0]["section"][6]["buttons"][0]["button"][1]["text"] = "Contact";
