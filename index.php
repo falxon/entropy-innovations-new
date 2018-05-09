da81177b252a5117bf122b2a6f47b03d9bb6cf70
Ember <falconsflight@riseup.net>
2018-05-09 17:26:52 +0100

    added https redirect

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}
+
 require 'modules/mustache/src/Mustache/Autoloader.php';
 Mustache_Autoloader::register();
 
