f2ef20ea87631ea0abd71152a1c7cfa359c30681
Ember Ashdown <emberashdown@gmail.com>
2018-12-29 17:12:18 +0000

    added extra portfolio item

--- php-include/portfolio.inc.php
+++ php-include/portfolio.inc.php
@@ -7,16 +7,24 @@ $portfolio["nav_item"][2]["sr_active"][0]["(current)"] = "(current)";
 $portfolio["header"][0]["class"] = "short-header";
 $portfolio["portfolio"][0]["title"] = "Portfolio";
 $portfolio["portfolio"][0]["paragraph"] = "Here are some examples of our work for previous clients.";
-$portfolio["portfolio"][0]["site"][0]["image"][0]["url"] = "https://dianabeavenphysiotherapy.com/";
-$portfolio["portfolio"][0]["site"][0]["image"][0]["src"] = "https://image.ibb.co/gfaUN7/physio.png";
-$portfolio["portfolio"][0]["site"][0]["image"][0]["alt"] = "Diana Beaven Physiotherapy";
-$portfolio["portfolio"][0]["site"][0]["text"][0]["content"] = "A website for a physiotherapy business. It includes information about treatments, a contact form and user-editable information to allow the client to update the site when information changes.";
-$portfolio["portfolio"][0]["site"][0]["text"][0]["url"] = "https://dianabeavenphysiotherapy.com/";
+
+$portfolio["portfolio"][0]["site"][0]["image"][0]["url"] = "https://actionfortranshealth.org/";
+$portfolio["portfolio"][0]["site"][0]["image"][0]["src"] = "images/ath-preview.png";
+$portfolio["portfolio"][0]["site"][0]["image"][0]["alt"] = "Action for Trans Health London";
+$portfolio["portfolio"][0]["site"][0]["text"][0]["content"] = "A website for Action for Trans Health London, a transgender rights organisation, allowing the group to receive donations, post news and articles, run campaigns, host resources, and link to their social media.";
+$portfolio["portfolio"][0]["site"][0]["text"][0]["url"] = "https://actionfortranshealth.org/";
 $portfolio["portfolio"][0]["site"][0]["text"][0]["button"] = "See More";
 
-$portfolio["portfolio"][0]["site"][1]["image"][0]["url"] = "https://human-in-nature.org.uk/";
-$portfolio["portfolio"][0]["site"][1]["image"][0]["src"] = "https://image.ibb.co/ivz05d/nature.png";
-$portfolio["portfolio"][0]["site"][1]["image"][0]["alt"] = "Human-In-Nature";
-$portfolio["portfolio"][0]["site"][1]["text"][0]["content"] = "A website for a photographer and artist to sell work, and write blog posts. It features multiple galleries, a blog section, and the facility for the client to add, modify, and delete photos, drawings and blog posts. It also features a contact form.";
-$portfolio["portfolio"][0]["site"][1]["text"][0]["url"] = "https://human-in-nature.org.uk/";
+$portfolio["portfolio"][0]["site"][1]["image"][0]["url"] = "https://dianabeavenphysiotherapy.com/";
+$portfolio["portfolio"][0]["site"][1]["image"][0]["src"] = "https://image.ibb.co/gfaUN7/physio.png";
+$portfolio["portfolio"][0]["site"][1]["image"][0]["alt"] = "Diana Beaven Physiotherapy";
+$portfolio["portfolio"][0]["site"][1]["text"][0]["content"] = "A website for a physiotherapy business. It includes information about treatments, a contact form and user-editable information to allow the client to update the site when information changes.";
+$portfolio["portfolio"][0]["site"][1]["text"][0]["url"] = "https://dianabeavenphysiotherapy.com/";
 $portfolio["portfolio"][0]["site"][1]["text"][0]["button"] = "See More";
+
+$portfolio["portfolio"][0]["site"][2]["image"][0]["url"] = "https://human-in-nature.org.uk/";
+$portfolio["portfolio"][0]["site"][2]["image"][0]["src"] = "https://image.ibb.co/ivz05d/nature.png";
+$portfolio["portfolio"][0]["site"][2]["image"][0]["alt"] = "Human-In-Nature";
+$portfolio["portfolio"][0]["site"][2]["text"][0]["content"] = "A website for a photographer and artist to sell work, and write blog posts. It features multiple galleries, a blog section, and the facility for the client to add, modify, and delete photos, drawings and blog posts. It also features a contact form.";
+$portfolio["portfolio"][0]["site"][2]["text"][0]["url"] = "https://human-in-nature.org.uk/";
+$portfolio["portfolio"][0]["site"][2]["text"][0]["button"] = "See More";
